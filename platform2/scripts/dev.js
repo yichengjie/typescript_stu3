7e3f6604ebed06401051bf87bb39347598307658
yichengjie <626659321@qq.com>
2017-07-09 11:37:59 +0800

    s

---
+++ platform2/scripts/dev.js
@@ -0,0 +1,29 @@
+'use strict';
+let webpack = require('webpack') ;
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+
+let config = require('../client/config/dev.js') ;
+let compiler = webpack(config);
+compiler.run((err, stats) => {
+    if (err) {
+    console.info(err);
+    return ; 
+    }
+    const messages = formatWebpackMessages(stats.toJson({}, true));
+    if (messages.errors.length) {
+    console.info(messages.errors.join('\n\n'));
+    return ;
+    }
+    if (process.env.CI && messages.warnings.length) {
+    console.log(
+        chalk.yellow(
+        '\nTreating warnings as errors because process.env.CI = true.\n' +
+            'Most CI servers set it automatically.\n'
+        )
+    );
+    console.info(messages.warnings.join('\n\n'));
+    return ;
+    }
+    console.info('compile success') ;
+    return true ;
+});
---
+++ platform2/scripts/tsc.js
@@ -0,0 +1,3 @@
+let typescript = require('typescript') ;
+
+typescript() ;
