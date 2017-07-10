ddd34527f98abe364f5f7cfd69232cb9003daba2
yichengjie <626659321@qq.com>
2017-07-10 11:29:03 +0800

    调整完price-jsx目录

---
+++ price-jsx/server/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var path = require('path') ;
+var port = 9090 ;
+var CategoryService = require('./category/category4.js') ;
+
+let publicPath = path.resolve(__dirname,'../public') ;
+let jspPath = path.resolve(__dirname,'../jsp') ;
+let distPath = path.resolve(__dirname,'../dist') ;
+
+app.use('/public',express.static(publicPath));
+app.use(express.static(jspPath));
+app.use('/dist',express.static(distPath));
+
+
+//getAllCategoryStaticData 
+app.get('/api/queryAllCategory4',function(req, res){
+  let category4List = CategoryService.queryAllCategory4() ;
+  let retData = {
+      flag:true,
+      data:category4List
+  } ;
+  res.json(retData);
+}) ;
+
+
+//queryCategory4ById 
+app.get('/api/queryCategory4ById',function(req, res){
+  let id = req.query.id ;
+  console.info('query url id is : ' + id) ;
+  let category4 = CategoryService.queryCategory4ById(id) ;
+  let retData = {
+      flag:true,
+      data:category4,
+  } ;
+  res.json(retData);
+}) ;
+
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
